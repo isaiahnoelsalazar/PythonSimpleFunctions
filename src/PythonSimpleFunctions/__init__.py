b8ff8ebb0bd1cc0a7d021c65fcfac4bf0c43ab72
isaiahnoelsalazar <isaiahnoelsalazar474@gmail.com>
2026-01-12 12:00:30 +0800

    new: add builder_for_flask_jsonify

--- src/PythonSimpleFunctions/__init__.py
+++ src/PythonSimpleFunctions/__init__.py
@@ -1,3 +1,4 @@
+from .builder_for_flask_jsonify import bake
 from .check import has_numbers, has_numbers2, has_spaces, has_spaces2, has_symbols, has_symbols2
 from .cipher import caesar_cipher, decipher_caesar_cipher, decipher_keyword_cipher, keyword_cipher, transposition_cipher
 from .convert import base64_to_string, string_to_base64
